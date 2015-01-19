3a7b959be8f871e5d2a3bbad6a12c82aec22a1c8
unknown <71476@NY-ESCHEMBOR-L1.INTERNAL.PAULWEISS.com>
2015-01-19 16:49:15 -0500

    Back to fastclick

--- js/main.js
+++ js/main.js
@@ -143,12 +143,12 @@ function handlekeyclick (k) {
 	}
 }
 
-//$(function() {
-//	FastClick.attach(document.body);
-//});
-
-var b = document.getElementById('btn1');
-var h = new Hammer(b);
-h.on ('tap', function(ev) {
-	handlekeyclick ("1");
+$(function() {
+	FastClick.attach(document.body);
 });
+
+//var b = document.getElementById('btn1');
+//var h = new Hammer(b);
+//h.on ('tap', function(ev) {
+//	handlekeyclick ("1");
+//});
